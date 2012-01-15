c5f98146f637df1fcc1e1d4567bc051d09e14b64
Scott Lawrence <bytbox@gmail.com>
2012-01-14 19:01:26 -0500

    Some class stubs, and Vector implementation

--- gleam.cc
+++ gleam.cc
@@ -1,3 +1,64 @@
+#include <iostream>
+using namespace std;
+
+#include <png.h>
+
+template <int D>
+class Vector {
+public:
+	Vector<D>() {
+		for (int i=0; i<D; i++)
+			d[i] = 0;
+	}
+
+	Vector<D> operator+(Vector<D> &v) {
+		Vector<D> r;
+		for (int i=0; i<D; i++)
+			r.d[i] = d[i] + v[i];
+		return r;
+	}
+
+	void operator+=(Vector<D> &v) {
+		for (int i=0; i<D; i++)
+			d[i] += v[i];
+	}
+
+	Vector<D> operator*(double f) {
+		Vector<D> r;
+		for (int i=0; i<D; i++)
+			r.d[i] = d[i]*f;
+		return r;
+	}
+
+	void operator*=(double f) {
+		for (int i=0; i<D; i++)
+			d[i] *= f;
+	}
+private:
+	double d[D];
+};
+
+typedef Vector<3> Vec;
+
+template <int D>
+class Ray {
+public:
+
+private:
+	double freq;
+	Vector<D> dir;
+	Vector<D> dest;
+};
+
+template <int D>
+class Camera {
+public:
+
+private:
+	
+};
+
 int main() {
+	Vec v;
 	return 0;
 }
